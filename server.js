2c5864d9570043c6f5d2d7a34a5b6cc90979a719
jthicks91 <jordanthicks91@gmail.com>
2019-05-14 19:31:13 -0700

    setup server file, almost completed CRUD operations for users. All custom middleware built for users.

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const express = 'express';
-
+const express = require("express");
 const server = express();
+const user_Router = require("./users/userRouter");
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
+server.use(express.json());
 
 //custom middleware
+server.use(logger);
+
+server.use("/api/users", user_Router);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 function logger(req, res, next) {
-
-};
+  const { path } = req;
+  const timeStamp = Date.now();
+  const log = { path, timeStamp };
+  console.log(`${req.method} Request`, log);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,105 @@
-const express = 'express';
+const express = require("express");
+const users = require("../users/userDb.js");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const USERPOSTCHECK = [validateUserId, validatePost];
 
+router.post("/", validatePost, async (req, res) => {
+  try {
+    const user = await users.insert(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: "error adding post"
+    });
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post("/:id/posts", USERPOSTCHECK, async (req, res) => {
+  const userInfo = { ...req.body, user_id: req.params.id };
+  try {
+    const post = await users.insert(userInfo);
+    res.status(201).json(post);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: "Error"
+    });
+  }
 });
 
-router.get('/', (req, res) => {
-
+router.get("/", async (req, res) => {
+  try {
+    const user = await users.get(req.query);
+    res.status(200).json(user);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the users"
+    });
+  }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", validateUserId, async (req, res) => {
+  res.status(200).json(req.hub);
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get("/:id/posts", validateUserId, async (req, res) => {
+  try {
+    const posts = await users.getUserPosts(req.params.id);
+    res.status(200).json(posts);
+  } catch (error) {
+    // log error to server
+    console.log(error);
+    res.status(500).json({
+      message: "Error getting the posts for the user"
+    });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validateUserId, async (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", validateUserId, async (req, res) => {});
 
 //custom middleware
-
-function validateUserId(req, res, next) {
-
-};
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const userId = await users.getById(id);
+    if (userId) {
+      req.user = userId;
+      next();
+    } else {
+      next({ message: "user with id: ${id} could not be found" });
+      // res.status(404).json({ message: "Hub not found; invalid id" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Failed to validate user with that id" });
+  }
+}
 
 function validateUser(req, res, next) {
-
-};
+  if (!req.body) {
+    res.status(400).json({ message: "missing user data" });
+    next();
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing name field is required" });
+    next();
+  }
+}
 
 function validatePost(req, res, next) {
-
-};
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" });
+    next();
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  }
+}
 
 module.exports = router;
