d9b9af91aa45174983e1a26c9eae920f9f230ba2
jthicks91 <jordanthicks91@gmail.com>
2019-05-16 18:32:28 -0700

    Deplyoment to Heroku

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
 const server = require("./server.js");
 
-server.listen(4000, () => {
-  console.log("\n* Server Running on http://localhost:4000 *\n");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`\n* Server Running on port ${PORT} *\n`);
 });
